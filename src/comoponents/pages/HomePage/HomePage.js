f3ded872807fecf5b098069a87d49eb4d20fead3
sainisarita <123sainisarita@gmail.com>
2023-06-03 16:04:24 +0530

    changes

--- src/comoponents/pages/HomePage/HomePage.js
+++ src/comoponents/pages/HomePage/HomePage.js
@@ -2,12 +2,13 @@ import React from 'react'
 import MyNavbar from './components/MyNavbar/MyNavbar'
 import Header from './components/Header/Header'
 import Card from './components/Card/Card'
+import './HomePage.css'
 
 
 const HomePage = () => {
   return (
 
-    <div className="header">
+    <div className="homePage">
         <Header/>
     <MyNavbar/>
     <Card/>
--- src/comoponents/pages/HomePage/components/Card/Card.js
+++ src/comoponents/pages/HomePage/components/Card/Card.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import './Card.css';
 import Ring from '../../images/ring.webp';
+import bag from '../../images/bag.jpg';
 
 const Card = () => {
   return (
     <div className="card">
-        <div className="card-image">
-      <img src={Ring} alt="My Photo"  />
+      <div className="card-image">
+        <img src={Ring} alt="ring" />
+      </div>
+      <div className="card-content">
+        <h2>IN STORES & ONLINE NOW</h2>
+        <span className="forever">Forever</span>
+        <span className="yours">Yours</span>
+        <h1>30% OFF - on your first purchase!</h1>
+      </div>
+      <div className="card-list">
+        <a href="#" className="child-card">
+          <div className="card-image">
+            <img src={bag} alt="My Photo" />
+          </div>
+          <div className="card-content">
+            <h2>Free Shipping</h2>
+            <p>Free shipping returns within 14 days for all sales.</p>
+          </div>
+        </a>
+        <a href="#" className="child-card">
+          <div className="card-image">
+            <img src={bag} alt="My Photo" />
+          </div>
+          <div className="card-content">
+            <h2>Free Shipping</h2>
+            <p>Free shipping returns within 14 days for all sales.</p>
+          </div>
+        </a>
+        <a href="#" className="child-card">
+          <div className="card-image">
+            <img src={bag} alt="My Photo" />
+          </div>
+          <div className="card-content">
+            <h2>Free Shipping</h2>
+            <p>Free shipping returns within 14 days for all sales.</p>
+          </div>
+        </a>
+      </div>
+      
     </div>
-        
-    </div>
-       
-     
-    
-    
   );
 };
 
