281151a2bee456889a5e8a39c4d78f863e0432bc
sainisarita <123sainisarita@gmail.com>
2023-06-28 10:05:58 +0530

    new commopnent added

--- src/comoponents/pages/HomePage/components/ArmaaniQuotes/ArmaaniQuotes.js
+++ src/comoponents/pages/HomePage/components/ArmaaniQuotes/ArmaaniQuotes.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
+import { Fade } from "react-reveal";
 import "./ArmaaniQuotes.css";
 import ArmaaniImage from "../../images/armaaniImage.jpg";
+import ArmaaniEaringImage from "../../images/earing1.jpg";
 
 const ArmaaniQuotes = () => {
+  const [scrollDirection, setScrollDirection] = useState("");
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      if (currentScrollPos > 0) {
+        setScrollDirection(currentScrollPos > scrollPosition ? "down" : "up");
+      }
+      setScrollPosition(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
+
   return (
     <div className="armaani-quotes">
       <Card className="custom-card">
-        <div className="card-image-container">
-          <div className="blank-card"></div> {/* Blank card behind the image */}
-          <Card.Img variant="top" src={ArmaaniImage} className="card-image" />
+        <div className="blank-card"></div>
+        {/* Blank card behind the image */}
+        <Card.Img variant="top" src={ArmaaniImage} className="card-image" />
+        <Fade duration={500} bottom={scrollDirection === "down"} top={scrollDirection === "up"}>
+          <Card.Img
+            variant="top"
+            src={ArmaaniEaringImage}
+            className="armaani-earing-image"
+          />
+        </Fade>
+        <div className="blockquotes">
+          <blockquote className="quote">
+          "To create something exceptional,
+            {"\n"}your mindset must be
+            {"\n"}relentlessly focused
+            {"\n"}on the smallest detail."
+            <br />
+          </blockquote>
+          <p className="author">——— Giorgio Armani</p>
         </div>
       </Card>
-    </div>
+      </div>
   );
 };
 
 export default ArmaaniQuotes;
+
