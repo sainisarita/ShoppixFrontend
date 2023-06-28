8501fd58cdf66e3838b7c60836945b1cd32ae8d8
sainisarita <123sainisarita@gmail.com>
2023-06-28 14:45:57 +0530

    fixed navbar

--- src/comoponents/pages/HomePage/components/MyNavbar/MyNavbar.js
+++ src/comoponents/pages/HomePage/components/MyNavbar/MyNavbar.js
@@ -2,12 +2,34 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MyNavbar.css';
+// import Header from '../Header/Header';
 
 const MyNavbar = () => {
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const navbar = document.getElementById('myNavbar');
+      const navbarOffsetTop = navbar.offsetTop;
+
+      if (scrollTop > navbarOffsetTop) {
+        setIsNavbarFixed(true);
+      } else {
+        setIsNavbarFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="myNavbar">
+    <div id="myNavbar" className={`myNavbar ${isNavbarFixed ? 'fixed-top' : ''}`}>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light" border-top border-dark>
         <Container>
           <Navbar.Brand href="#home">Shoppix</Navbar.Brand>
